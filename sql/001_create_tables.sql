cf8f1182d6dea8c88d7b463b757c7e59721affa3
Ondrej Mikle <ondrej.mikle@gmail.com>
2012-03-17 23:09:45 +0100

    Some SQL for table creation

--- sql/001_create_tables.sql
+++ sql/001_create_tables.sql
@@ -17,12 +17,14 @@ CREATE TABLE rrsig_rr (
     signature VARCHAR NOT NULL
 );
 
--- Table for As
-CREATE TABLE a_rr (
+-- Table for A and AAAA records
+CREATE TABLE aa_rr (
     id SERIAL PRIMARY KEY,
     domain VARCHAR(255) NOT NULL,
     ttl INTEGER NOT NULL,
-    rr INET NOT NULL,
+    addr INET NOT NULL,
     rrsig_id INTEGER REFERENCES rrsig_rr(id)
 );
 
+-- not using unique constraints yet
+-- CREATE UNIQUE INDEX aa_rr_idx on aa_rr (domain, addr, rrsig_id);
