3eddea4ee95dcf9f537e43ad80a76e735abf246d
Ondrej Mikle <ondrej.mikle@gmail.com>
2012-03-18 01:48:00 +0100

    Added table for DNSKEY and secure fields (DNSSEC validation result).

--- sql/001_create_tables.sql
+++ sql/001_create_tables.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS aa_rr;
+DROP TABLE IF EXISTS dnskey_rr;
 DROP TABLE IF EXISTS rrsig_rr;
 
 -- Table for RRSIGs
@@ -14,7 +15,7 @@ CREATE TABLE rrsig_rr (
     sig_inception TIMESTAMP NOT NULL,
     keytag INTEGER NOT NULL,
     signer VARCHAR(255) NOT NULL,
-    signature VARCHAR NOT NULL
+    signature BYTEA NOT NULL
 );
 
 CREATE INDEX rrsig_rr_domain_idx ON rrsig_rr (domain);
@@ -22,6 +23,7 @@ CREATE INDEX rrsig_rr_domain_idx ON rrsig_rr (domain);
 -- Table for A and AAAA records
 CREATE TABLE aa_rr (
     id SERIAL PRIMARY KEY,
+    secure BOOLEAN,
     domain VARCHAR(255) NOT NULL,
     ttl INTEGER NOT NULL,
     addr INET NOT NULL,
@@ -29,4 +31,19 @@ CREATE TABLE aa_rr (
 );
 
 CREATE INDEX aa_rr_domain_idx ON aa_rr (domain);
+
+-- Table for DNSKEY
+CREATE TABLE dnskey_rr (
+    id SERIAL PRIMARY KEY,
+    secure BOOLEAN,
+    domain VARCHAR(255) NOT NULL,
+    ttl INTEGER NOT NULL,
+    flags SMALLINT NOT NULL,
+    protocol SMALLINT NOT NULL,
+    algo SMALLINT NOT NULL,
+    pubkey BYTEA NOT NULL,
+    rrsig_id INTEGER REFERENCES rrsig_rr(id)
+);
+
+CREATE INDEX dnskey_rr_domain_idx ON dnskey_rr (domain);
 
