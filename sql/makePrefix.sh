21cca1dde278d2ef80ba305d1e58b8c2413296d7
Ralph Holz (TUM) <holz@net.in.tum.de>
2012-04-17 16:24:51 +0200

    the wonderful world of sed has taught me yet another thing

--- sql/makePrefix.sh
+++ sql/makePrefix.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 PREFIX=$1
-cat create_tables_template.sql | sed 's/.*TABLE.*aa\_rr/'$PREFIX'\.aa\_rr/g' \
-| sed 's/.*TABLE.*dnskey\_rr/'$PREFIX'\.dnskey\_rr/g' \
-| sed 's/.*TABLE.*ns\_rr/'$PREFIX'\.ns\_rr/g' \
-| sed 's/.*TABLE.*ds\_rr/'$PREFIX'\.ds\_rr/g' \
-| sed 's/.*TABLE.*soa\_rr/'$PREFIX'\.soa\_rr/g' \
-| sed 's/.*TABLE.*sshfp\_rr/'$PREFIX'\.sshfp\_rr/g' \
-| sed 's/.*TABLE.*txt\_rr/'$PREFIX'\.txt\_rr/g' \
-| sed 's/.*TABLE.*spf\_rr/'$PREFIX'\.spf\_rr/g' \
-| sed 's/.*TABLE.*nsec3param\_rr/'$PREFIX'\.nsec3param\_rr/g' \
-| sed 's/.*TABLE.*mx\_rr/'$PREFIX'\.mx\_rr/g' \
-| sed 's/.*TABLE.*nsec\_rr/'$PREFIX'\.nsec\_rr/g' \
-| sed 's/.*TABLE.*nsec3\_rr/'$PREFIX'\.nsec3\_rr/g' \
-| sed 's/.*TABLE.*rrsig\_rr/'$PREFIX'\.rrsig\_rr/g' \
-| sed 's/OURSCHEMA/'$PREFIX'/' > $2
+cat create_tables_template.sql | sed 's/TABLE\ aa\_rr/TABLE\ '$PREFIX'\.aa\_rr/g' \
+| sed 's/TABLE\ dnskey\_rr/TABLE\ '$PREFIX'\.dnskey\_rr/g' \
+| sed 's/TABLE\ ns\_rr/TABLE\ '$PREFIX'\.ns\_rr/g' \
+| sed 's/TABLE\ ds\_rr/TABLE\ '$PREFIX'\.ds\_rr/g' \
+| sed 's/TABLE\ soa\_rr/TABLE\ '$PREFIX'\.soa\_rr/g' \
+| sed 's/TABLE\ sshfp\_rr/TABLE\ '$PREFIX'\.sshfp\_rr/g' \
+| sed 's/TABLE\ txt\_rr/TABLE\ '$PREFIX'\.txt\_rr/g' \
+| sed 's/TABLE\ spf\_rr/TABLE\ '$PREFIX'\.spf\_rr/g' \
+| sed 's/TABLE\ nsec3param\_rr/TABLE\ '$PREFIX'\.nsec3param\_rr/g' \
+| sed 's/TABLE\ mx\_rr/TABLE\ '$PREFIX'\.mx\_rr/g' \
+| sed 's/TABLE\ nsec\_rr/TABLE\ '$PREFIX'\.nsec\_rr/g' \
+| sed 's/TABLE\ nsec3\_rr/TABLE\ '$PREFIX'\.nsec3\_rr/g' \
+| sed 's/TABLE\ rrsig\_rr/TABLE\ '$PREFIX'\.rrsig\_rr/g' \
+| sed 's/OURSCHEMA/'$PREFIX'/'
