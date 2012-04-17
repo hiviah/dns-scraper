9bc3e38e9062942c2031a4c2810a3b7db7374628
Ralph Holz (TUM) <holz@net.in.tum.de>
2012-04-17 16:08:57 +0200

    added

---
+++ sql/makePrefix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+PREFIX=17Apr2012
+cat create_tables_template.sql | sed 's/aa\_rr/'$PREFIX'\.aa\_rr/g' \
+| sed 's/dnskey\_rr/'$PREFIX'\.dnskey\_rr/g' \
+| sed 's/ns\_rr/'$PREFIX'\.ns\_rr/g' \
+| sed 's/ds\_rr/'$PREFIX'\.ds\_rr/g' \
+| sed 's/soa\_rr/'$PREFIX'\.soa\_rr/g' \
+| sed 's/sshfp\_rr/'$PREFIX'\.sshfp\_rr/g' \
+| sed 's/txt\_rr/'$PREFIX'\.txt\_rr/g' \
+| sed 's/spf\_rr/'$PREFIX'\.spf\_rr/g' \
+| sed 's/nsec3param\_rr/'$PREFIX'\.nsec3param\_rr/g' \
+| sed 's/mx\_rr/'$PREFIX'\.mx\_rr/g' \
+| sed 's/nsec\_rr/'$PREFIX'\.nsec\_rr/g' \
+| sed 's/nsec3\_rr/'$PREFIX'\.nsec3\_rr/g' \
+| sed 's/rrsig\_rr/'$PREFIX'\.rrsig\_rr/g' \
+| sed 's/OURSCHEMA/'$PREFIX'/'
